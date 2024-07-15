6b0a334cbc1b0d29787d248419498ffbe3b85fc9
Aditya Prashar <adityaprashar03@gmail.com>
2024-07-15 23:37:31 +0530

    OOP2

---
+++ src/com/aditya/packages/a/Greetings.java
@@ -0,0 +1,8 @@
+package com.aditya.packages.a;
+import static com.aditya.packages.b.Message.message;
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        message();
+    }
+}
---
+++ src/com/aditya/packages/b/Greetings.java
@@ -0,0 +1,7 @@
+package com.aditya.packages.b;
+
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println(" I am awesome");
+    }
+}
